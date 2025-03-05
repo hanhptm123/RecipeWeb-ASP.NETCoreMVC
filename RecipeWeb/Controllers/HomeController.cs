f4a2cec0fff2fbe0ae0cc23842a7c9ed9a18814e
Loc24102004 <phuonghuugialoc@siu.edu.vn>
2025-03-05 15:03:43 +0800

    Add Layout Admin

--- RecipeWeb/Controllers/HomeController.cs
+++ RecipeWeb/Controllers/HomeController.cs
@@ -17,7 +17,10 @@ namespace RecipeWeb.Controllers
         {
             return View();
         }
-
+        public IActionResult AdminLayout()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
